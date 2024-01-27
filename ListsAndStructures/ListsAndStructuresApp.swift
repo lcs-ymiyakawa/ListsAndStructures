385a5f2dbb5a500a3f95a1805a86aba500dc0fda
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-27 09:59:30 -0500

    Initial Commit

---
+++ ListsAndStructures/ListsAndStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsAndStructuresApp.swift
+//  ListsAndStructures
+//
+//  Created by 宮川義之助 on 2024/01/27.
+//
+
+import SwiftUI
+
+@main
+struct ListsAndStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
